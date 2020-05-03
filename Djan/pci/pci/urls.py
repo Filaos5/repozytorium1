696eb7d846e133973dad67b77597c70afa7b2d94
Filaos5 <62643533+Filaos5@users.noreply.github.com>
2020-05-03 20:23:52 +0200

    aplikacja cala

---
+++ Djan/pci/pci/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+#url(r'^accounts/login/$','uzytkownik.views.login'),
